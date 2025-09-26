3af3578bcca18296ba24878fdbdb38551dbae6fc
celesdiaz <celes20.diaz.cd@gmail.com>
2025-09-26 20:35:04 -0300

    Dominio y Negocio de Clientes v1

---
+++ TPWeb_equipo-11b/dominio/Clientes.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace dominio
+{
+    public class Clientes
+    {
+        public string Documento { get; set; }
+        public string Nombre { get; set; }
+        public string Apellido { get; set; }
+        public string Email { get; set; }
+        public string Direccion { get; set; }
+        public string Ciudad { get; set; }
+        public int CP { get; set; }
+    }
+}
